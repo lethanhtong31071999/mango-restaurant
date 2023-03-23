936551453f5406fd72f2e2e5b47efdcda989aa0b
VN\Roy.Le <roy.le@tpfgroup.cc>
2023-03-23 11:27:53 +0700

    Service - Setup - IProductRepo Dto

---
+++ Mango.Services.Product.API/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+
+namespace Mango.Services.ProductAPI.Repository.IRepository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto product);
+        Task<bool> DeleteProduct(int productId);
+    }
+}
