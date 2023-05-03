f67bc11318b45c88d0a43d16d97e7eff7994c54a
Roy Le <lethanhtong31071999@gmail.com>
2023-05-03 17:37:41 +0700

    Identity - set up - and remove docker debug

---
+++ Mango.Services.IdentityServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mango.Services.IdentityServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
